f0a60bf2f2c0d6d2693e1b93c7faaf3f8ed849dc
deana17 <91081633+deana17@users.noreply.github.com>
2021-12-30 11:14:53 +0200

    SurveyList antd

--- src/pages/SurveyList/index.jsx
+++ src/pages/SurveyList/index.jsx
@@ -1,41 +1,64 @@
-import style from './style.module.css'
+import style from "./style.module.css";
+import { Table } from "antd";
 
-const data = [{
+const dataSource = [
+  {
     id: 1,
-    name: 'PCL-5',
+    name: "PCL-5",
     questionsAmount: 5,
-    date: '16-01-2021'
-
+    date: "16-01-2021",
   },
   {
     id: 2,
-    name: 'GAD',
+    name: "GAD",
     questionsAmount: 20,
-    date: '01-06-2021'
-
+    date: "01-06-2021",
   },
   {
     id: 3,
-    name: 'PST-420-BLAZEIT',
+    name: "PST-420-BLAZEIT",
     questionsAmount: 200,
-    date: '01-01-2022'
-
+    date: "01-01-2022",
   },
   {
     id: 4,
-    name: 'THC',
+    name: "THC",
     questionsAmount: 55,
-    date: '01-01-2022'
+    date: "01-01-2022",
+  },
+];
 
-  }
-]
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "questionsAmount",
+    dataIndex: "questionsAmount",
+    key: "questionsAmount",
+  },
+  {
+    title: "date",
+    dataIndex: "date",
+    key: "date",
+  },
+];
 
 function SurveyList(props) {
-    return (
-        <div>
-            <h1>SurveyList</h1>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "block",
+        width: 700,
+        padding: 30,
+      }}
+    >
+      <h1>Survey List</h1>
+      <Table dataSource={dataSource} columns={columns} />;
+    </div>
+  );
 }
 
-export default SurveyList
+export default SurveyList;
