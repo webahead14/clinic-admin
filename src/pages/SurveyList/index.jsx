c1b7683648cdd6ee9915af359ce1b4ea0a2bb154
Mario Saliba <mario.saliba98@gmail.com>
2021-12-30 10:28:39 +0200

    Update index.jsx

--- src/pages/SurveyList/index.jsx
+++ src/pages/SurveyList/index.jsx
@@ -1,5 +1,35 @@
 import style from './style.module.css'
 
+const data = [{
+    id: 1,
+    name: 'PCL-5',
+    questionsAmount: 5,
+    date: '16-01-2021'
+
+  },
+  {
+    id: 2,
+    name: 'GAD',
+    questionsAmount: 20,
+    date: '01-06-2021'
+
+  },
+  {
+    id: 3,
+    name: 'PST-420-BLAZEIT',
+    questionsAmount: 200,
+    date: '01-01-2022'
+
+  },
+  {
+    id: 4,
+    name: 'THC',
+    questionsAmount: 55,
+    date: '01-01-2022'
+
+  }
+]
+
 function SurveyList(props) {
     return (
         <div>
