171213aabcf708af991ac50f30eccb3ee1a3aaca
julio1703 <julio.elias22@gmail.com>
2022-01-03 16:41:02 +0200

    .

--- src/pages/ClientPage/index.jsx
+++ src/pages/ClientPage/index.jsx
@@ -1,9 +1,14 @@
 import style from "./style.module.css";
 import options from "../../utils/allSurveys";
+import { Card, Descriptions, Divider } from "antd";
+
 function ClientPage(props) {
   return (
     <div>
       <h1>ClientPage</h1>
+      <Card>
+        <Descriptions title="User Info"></Descriptions>
+      </Card>
     </div>
   );
 }
