666275751de37cae8e35e84fef2bf93bfd39a061
deana17 <91081633+deana17@users.noreply.github.com>
2022-01-05 15:12:39 +0200

    added a line chart only

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,38 @@
-import style from './style.module.css'
-import { Button } from 'antd'
+import React from "react";
+import style from "./style.module.css";
+import { Button } from "antd";
+import { Line } from "@ant-design/charts";
+
+//data for line chart
+const data = [
+  { year: "1991", value: 3 },
+  { year: "1992", value: 4 },
+  { year: "1993", value: 3.5 },
+  { year: "1994", value: 5 },
+  { year: "1995", value: 4.9 },
+  { year: "1996", value: 6 },
+  { year: "1997", value: 7 },
+  { year: "1998", value: 9 },
+  { year: "1999", value: 13 },
+];
+
+const config = {
+  data,
+  xField: "year",
+  yField: "value",
+  point: {
+    size: 5,
+    shape: "diamond",
+  },
+};
 
 function Home(props) {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Button type="primary">Click me bro!</Button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+      <Line {...config} />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
