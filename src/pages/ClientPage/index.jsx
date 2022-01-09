3fb3db47e2f2fc52f07867afd1340bc401e20aae
julio1703 <julio.elias22@gmail.com>
2022-01-09 13:51:32 +0200

    .

--- src/pages/ClientPage/index.jsx
+++ src/pages/ClientPage/index.jsx
@@ -1,7 +1,7 @@
 import style from "./style.module.css";
 import "./style.css";
+import axios from "axios";
 import React from "react";
-import options from "../../utils/allSurveys";
 import { Card, Tabs, Descriptions, Divider, Collapse } from "antd";
 import {
   CloseCircleTwoTone,
@@ -104,106 +104,119 @@ const Client = {
 };
 
 function ClientPage(props) {
-  // const [client, setClient] = React.useState({}); //this is for when we do the fetch from backend
-  // const [surveys, setSurveys] = React.useState([]);
+  const [client, setClient] = React.useState({}); //this is for when we do the fetch from backend
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
 
-  const tabsArray = new Array(8).fill(1);
-  return (
+    axios
+      .get("http://localhost:4000/api/clinic/client/1", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((data) => {
+        setClient((client) => (client = data.data));
+      });
+  }, []);
+
+  const tabsArray = new Array(8).fill((props) => (
+    <TabPane {...props}>{props.children}</TabPane>
+  ));
+  return JSON.stringify(client) !== "{}" ? (
     <div>
       <h1>ClientPage</h1>
-      <div className={style.cardStyle}>
-        <Card>
-          <Descriptions title="Patient Info">
-            <Descriptions.Item label="ID">{Client.id}</Descriptions.Item>
-            <Descriptions.Item label="Name">{Client.name}</Descriptions.Item>
-            <Descriptions.Item label="Government ID">
-              {Client.govId}
-            </Descriptions.Item>
-            <Descriptions.Item label="Gender">
-              {Client.gender}
-            </Descriptions.Item>
-            <Descriptions.Item label="Phone">{Client.phone}</Descriptions.Item>
-            <Descriptions.Item label="Email">{Client.email}</Descriptions.Item>
-          </Descriptions>
+      <Card>
+        <Descriptions title="Patient Info">
+          <Descriptions.Item label="ID">{client.id}</Descriptions.Item>
+          <Descriptions.Item label="Name">{client.name}</Descriptions.Item>
+          <Descriptions.Item label="Government ID">
+            {client.govId}
+          </Descriptions.Item>
+          <Descriptions.Item label="Gender">{client.gender}</Descriptions.Item>
+          <Descriptions.Item label="Phone">{client.phone}</Descriptions.Item>
+          <Descriptions.Item label="Email">{client.email}</Descriptions.Item>
+        </Descriptions>
 
-          <Divider></Divider>
+        <Divider></Divider>
 
-          <Descriptions title="Patient File">
-            <Descriptions.Item label="Protocol" span="2">
-              {Client.protocol.name}
-            </Descriptions.Item>
-            <Descriptions.Item>
-              <Collapse
-                style={{ position: "sticky", padding: "3px,16px" }}
-                ghost
+        <Descriptions title="Patient File">
+          <Descriptions.Item label="Protocol" span="2">
+            {client.protocol.name}
+          </Descriptions.Item>
+          <Descriptions.Item>
+            <Collapse style={{}} ghost>
+              <Panel
+                header="Treatment"
+                style={{ padding: 0 }}
+                className={style.test}
               >
-                <Panel header="Treatment" className={style.test}>
-                  <p>StartDate: {Client.treatment.startDate}</p>
-                  <p>Status: {Client.treatment.status}</p>
-                </Panel>
-              </Collapse>
-            </Descriptions.Item>
-            <Descriptions.Item label="Condition" span="2">
-              {Client.condition}
-            </Descriptions.Item>
-          </Descriptions>
+                <div style={{ position: "absolute" }}>
+                  <p>StartDate: {client.treatment.startDate.split("T")[0]}</p>
+                  <p>Status: {client.treatment.status}</p>
+                </div>
+              </Panel>
+            </Collapse>
+          </Descriptions.Item>
+          <Descriptions.Item label="Condition" span="2">
+            {client.condition}
+          </Descriptions.Item>
+        </Descriptions>
 
-          <Divider orientation="left">Survey</Divider>
+        <Divider orientation="left">Survey</Divider>
 
-          <Tabs defaultActiveKey="1">
-            {tabsArray.map((Element, index) => {
-              return (
-                <TabPane tab={`Week ${index + 1}`} key={`${index + 1}`}>
-                  <Descriptions>
-                    {Client.surveyProgress
-                      ? Client.surveyProgress
-                          .map((element, idx) => {
-                            if (element.week === `${index + 1}`) {
-                              return (
-                                <Descriptions.Item key={idx}>
-                                  <Collapse
-                                    accordion="true"
-                                    style={{ padding: "3px,16px" }}
-                                    ghost
-                                  >
-                                    <Panel header={element.name}>
-                                      <p>
-                                        Progress:
-                                        {element.isDone ? (
-                                          <CheckCircleTwoTone twoToneColor="#52c41a" />
-                                        ) : element.isPartiallyDone ? (
-                                          <LoadingOutlined />
-                                        ) : (
-                                          <CloseCircleTwoTone twoToneColor="#fc6161" />
-                                        )}
-                                      </p>
-                                      <p>
-                                        MissedDate:
-                                        {element.hasMissed ? (
-                                          <CheckCircleTwoTone twoToneColor="#52c41a" />
-                                        ) : (
-                                          <CloseCircleTwoTone twoToneColor="#fc6161" />
-                                        )}
-                                      </p>
-                                    </Panel>
-                                  </Collapse>
-                                </Descriptions.Item>
-                              );
-                            } else {
-                              return null;
-                            }
-                          })
-                          .filter((x) => x)
-                      : null}
-                  </Descriptions>
-                </TabPane>
-              );
-            })}
-          </Tabs>
-        </Card>
-      </div>
+        <Tabs defaultActiveKey="1">
+          {tabsArray.map((Element, index) => {
+            return (
+              <Element tab={`Week ${index + 1}`} key={`${index + 1}`}>
+                <Descriptions>
+                  {client.surveyProgress
+                    ? client.surveyProgress
+                        .map((element, idx) => {
+                          if (element.week === index + 1) {
+                            return (
+                              <Descriptions.Item key={idx}>
+                                <Collapse
+                                  accordion="true"
+                                  style={{ padding: "3px,16px" }}
+                                  ghost
+                                >
+                                  <Panel header={element.name}>
+                                    <p>
+                                      Progress:
+                                      {element.isDone ? (
+                                        <CheckCircleTwoTone twoToneColor="#52c41a" />
+                                      ) : element.isPartiallyDone ? (
+                                        <LoadingOutlined />
+                                      ) : (
+                                        <CloseCircleTwoTone twoToneColor="#fc6161" />
+                                      )}
+                                    </p>
+                                    <p>
+                                      MissedDate:
+                                      {element.hasMissed ? (
+                                        <CheckCircleTwoTone twoToneColor="#52c41a" />
+                                      ) : (
+                                        <CloseCircleTwoTone twoToneColor="#fc6161" />
+                                      )}
+                                    </p>
+                                  </Panel>
+                                </Collapse>
+                              </Descriptions.Item>
+                            );
+                          } else {
+                            return null;
+                          }
+                        })
+                        .filter((x) => x)
+                    : null}
+                </Descriptions>
+              </Element>
+            );
+          })}
+        </Tabs>
+      </Card>
     </div>
-  );
+  ) : null;
 }
 
 export default ClientPage;
