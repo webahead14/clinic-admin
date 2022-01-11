8cc3d5256ef7729e7e92e31ed7f1bac319f9d45e
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2022-01-11 15:54:12 +0200

    data

--- src/utils/api.js
+++ src/utils/api.js
@@ -30,6 +30,6 @@ export function postClient(data) {
     throw new Error("Please Choose A Protocol");
   } else {
     alert("Form Submitted");
-    return axios.post("http://localhost:4000/api/client/register", { data });
+    return axios.post("http://localhost:4000/api/client/register", data);
   }
 }
